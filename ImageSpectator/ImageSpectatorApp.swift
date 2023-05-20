29ea774922462e9df22139c0b538e1e50e221569
Keisuke Isono <reirou.k@gmail.com>
2023-05-21 08:11:40 +0900

    Create Xcode Project

---
+++ ImageSpectator/ImageSpectatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageSpectatorApp.swift
+//  ImageSpectator
+//
+//  Created by Keisuke Isono on 2023/05/21.
+//
+
+import SwiftUI
+
+@main
+struct ImageSpectatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
